f5269d014114de4dcdb138f3406ad53c4875b173
alexneo68 <tunning_brain@mail.ru>
2019-06-03 18:32:31 +0300

    add handle errors

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,19 @@ namespace App\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use App\Traits\ApiResponser;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Illuminate\Database\QueryException;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -46,6 +56,67 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($exception instanceof ValidationException){
+            $this->convertValidationExceptionToResponse($exception, $request);
+        }
+
+        if($exception instanceof ModelNotFoundException){
+            $model = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("Not found the {$model} instance", 404);
+        }
+
+        if($exception instanceof AuthenticationException){
+            $this->unauthenticated($request, $exception);
+        }
+
+        if($exception instanceof AuthorizationException){
+            return $this->errorResponse("Access denied", 403);
+        }
+
+        if($exception instanceof MethodNotAllowedHttpException){
+            return $this->errorResponse("The specified http method not allowed to this request", 405);
+        }
+
+        if($exception instanceof NotFoundHttpException){
+            return $this->errorResponse("The specified http URL not found on this server", 404);
+        }
+
+        if($exception instanceof HttpException){
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if($exception instanceof QueryException){
+            $errorCode = $exception->errorInfo[1];
+            if($errorCode == 1451){
+                return $this->errorResponse('This resource cannot be delete, because it related to another resource', 409);
+            }
+        }
+
+        if(config('app.debug')){
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Server side error', 500);
     }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse($exception->getMessage(), 401);
+    }
+
+    protected function convertValidationExceptionToResponse(ValidationException $exception, $request)
+    {
+        return $this->errorResponse($exception->errors(), $exception->status);
+    }
+
+
+    
+
 }
