54c02bd84c2d8e2b905472a122d6ba73696689a6
AlexNeo68 <tunning_brain@mail.ru>
2019-06-04 08:54:35 +0300

    implementing scopes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -65,10 +65,8 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
-
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
         $rules = [
             'email' => 'email|unique:users,email,'.$user->id,
             'password' => 'min:6|confirmed',
